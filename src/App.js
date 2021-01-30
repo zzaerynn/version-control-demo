b85cca725b5756b4c629308dd01fd7d64f3e026d
Alex Kuhn <alexckuhn@me.com>
2021-01-30 12:26:31 +0900

    Add timer component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import "./components/HelloMessage";
 import HelloMessage from "./components/HelloMessage";
-
+import Timer from "./components/Timer";
 
 function App() {
   return (
     <div className="App">
         <HelloMessage name="Everyone" />
         <HelloMessage name="There" />
+        <Timer />
     </div>
   );
 }

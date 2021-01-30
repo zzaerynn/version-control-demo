66155e9d63644c0a182c6680344f10f442843d46
Alex Kuhn <alexckuhn@me.com>
2021-01-30 12:41:45 +0900

    Add TodoApp

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
-import "./components/HelloMessage";
 import HelloMessage from "./components/HelloMessage";
 import Timer from "./components/Timer";
+import TodoApp from "./components/TodoApp";
 
 function App() {
   return (
     <div className="App">
         <HelloMessage name="Everyone" />
         <HelloMessage name="There" />
+        <br />
+        <h3>My Timer:</h3>
         <Timer />
+        <h3>My Todo App:</h3>
+        <TodoApp />
     </div>
   );
 }

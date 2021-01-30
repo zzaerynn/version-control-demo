05453b6ede858bd2b64f7e71a4191e64ad328ec4
Alex Kuhn <alexckuhn@me.com>
2021-01-30 12:10:41 +0900

    Make basic HelloMessage component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import React from "react"
 import './App.css';
+
+class HelloMessage extends React.Component {
+    render() {
+        return (
+            <div>
+                Hello {this.props.name}
+            </div>
+        );
+    }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <HelloMessage name="Everyone" />
     </div>
   );
 }
